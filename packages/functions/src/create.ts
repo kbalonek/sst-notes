e86f57738d0e7c163595cecd17deb6ab62baadcd
Kris Balonek <kris@vyntelligence.com>
2024-02-08 21:35:39 +0000

    API for for POST and GET

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,28 @@
+import * as uuid from "uuid";
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamoDb from "@notes/core/dynamodb";
+
+export const main = handler(async (event) => {
+    let data = {
+        content: "",
+        attachment: "",
+    }
+
+    if (event.body != null) {
+        data = JSON.parse(event.body);
+    }
+    const params = {
+        TableName: Table.Notes.tableName,
+        Item: {
+            userId: "123",
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    }
+    await dynamoDb.put(params);
+    return JSON.stringify(params.Item);
+}
+// "noteId":"7e3a3750-c2f3-11ee-878e-01ce009f2b30"
---
+++ stacks/ApiStack.ts
@@ -0,0 +1,26 @@
+import { Api, StackContext, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack }: StackContext) {
+    const { table } = use(StorageStack);
+
+    const api = new Api(stack, "Api", {
+        defaults: {
+            function: {
+                bind: [table],
+            }
+        },
+        routes: {
+            "POST /notes": "packages/functions/src/create.main",
+            "GET /notes/{id}": "packages/functions/src/get.main",
+        }
+    });
+
+    stack.addOutputs({
+        ApiEndpoint: api.url
+    });
+
+    return {
+        api
+    }
+}
